0bda2856dec5f2caff65dcfc4250c91d450991c1
kchen27 <kvc5823@psu.edu>
2020-08-28 19:29:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+temperature = float(input ("Enter temperature in Celcius:"))
+fahrenheit = ((temperature * (9/5))+32)
+print (f"{temperature} ° in Celsius is equivalent to {fahrenheit}° Fahrenheit.")
