4e178079c1b2e8ee1295dd55267862aaea6319a5
kchen27 <kvc5823@psu.edu>
2020-08-28 19:33:40 +0000

    spacing and spelling issues

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-temperature = float(input ("Enter temperature in celcius:"))
+temperature = float(input ("Enter temperature in celcius: "))
 fahrenheit = ((temperature * (9/5))+32)
 print (f"{temperature}° in Celcius is equivalent to {fahrenheit}° Fahrenheit.")
